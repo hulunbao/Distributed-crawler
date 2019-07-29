9e50ba0514addee9a4619bdb56c5827abda650ea
hulunbao <hulunbao@gmail.com>
2019-07-29 15:01:37 +0800

    改动

--- zhenai/parser/city.go
+++ zhenai/parser/city.go
@@ -13,7 +13,7 @@ func ParseCity(contents []byte) engine.ParserResult {
 
 	result := engine.ParserResult{}
 	for _, m := range matches {
-		result.Items = append(result.Items,"User"+string(m[2]))
+		result.Items = append(result.Items,"User "+string(m[2]))
 		result.Requests = append(result.Requests,engine.Request{
 			Url: string(m[1]),
 			ParserFunc: engine.NilParser,
