5bd21a75fcb2f3ca1f25e281d36443ab4763cef0
hulunbao <hulunbao@gmail.com>
2019-07-31 17:21:59 +0800

    修改遍历次数

--- zhenai/parser/citylist.go
+++ zhenai/parser/citylist.go
@@ -12,17 +12,12 @@ func ParseCityList(contents []byte) engine.ParserResult {
 	matches := cityListRe.FindAllSubmatch(contents, -1)
 
 	result := engine.ParserResult{}
-	limit := 2
 	for _, m := range matches {
 		result.Items = append(result.Items, "City "+string(m[2]))
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
 			ParserFunc: ParseCity,
 		})
-		limit--
-		if limit == 0 {
-			break
-		}
 	}
 	return result
 }
