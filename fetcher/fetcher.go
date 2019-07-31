46504061aa98fa5c42242c9199c9116b832605fd
hulunbao <hulunbao@gmail.com>
2019-07-31 17:02:41 +0800

    解决403问题

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -3,21 +3,25 @@ package fetcher
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
-	"io/ioutil"
-	"log"
-	"net/http"
 )
 
-func Fetch(url string) ([] byte,error){
+// Fetch 解析url　返回二进制body
+func Fetch(url string) ([]byte, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	req.Close = true
+
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
 
 	resp, err := client.Do(req)
@@ -34,12 +38,12 @@ func Fetch(url string) ([] byte,error){
 	//defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil,fmt.Errorf("wrong status code: %d",resp.StatusCode)
+		return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
 	}
 
 	bodyReader := bufio.NewReader(resp.Body)
 	e := determineEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader,e.NewDecoder())
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
 
 	return ioutil.ReadAll(utf8Reader)
 }
@@ -47,7 +51,7 @@ func Fetch(url string) ([] byte,error){
 func determineEncoding(r *bufio.Reader) encoding.Encoding {
 	bytes, err := r.Peek(1024)
 	if err != nil {
-		log.Printf("Fetcher error: %v",err)
+		log.Printf("Fetcher error: %v", err)
 		return unicode.UTF8
 	}
 	e, _, _ := charset.DetermineEncoding(bytes, "")
